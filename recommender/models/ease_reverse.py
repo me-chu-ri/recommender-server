10bbae2a43e8da38ae7dd744a476084c3708cc0a
Park-SeungWoo <tmd24710@naver.com>
2024-05-09 21:47:59 +0900

    chore: initial setting

---
+++ recommender/models/ease_reverse.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+class EASEr:
+    def __init__(self, _lambda):
+        self.B = None
+        self._lambda = _lambda
+
+    def train(self, X):
+        pass
+
+    def recommend(self, user_row):
+        pass
