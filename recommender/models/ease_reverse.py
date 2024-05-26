6591198f22afc1a5afbb532afa288f9405c41c94
Park-SeungWoo <tmd24710@naver.com>
2024-05-26 20:37:38 +0900

    feat: finish EASEr

--- recommender/models/ease_reverse.py
+++ recommender/models/ease_reverse.py
@@ -3,11 +3,16 @@ import numpy as np
 
 class EASEr:
     def __init__(self, _lambda):
-        self.B = None
+        self.B: np.ndarray = np.array([])
         self._lambda = _lambda
 
-    def fit(self, X):
-        pass
+    def fit(self, X: np.ndarray):
+        G: np.ndarray = X.T @ X
+        diag_indices = np.diag_indices_from(G.shape[0])
+        G[diag_indices] += self._lambda
+        P: np.ndarray = np.linalg.inv(G)
+        self.B = P / -np.diag(P)
+        self.B[diag_indices] = 0
 
-    def predict(self, user_row):
-        pass
+    def predict(self, user_row: np.ndarray) -> np.ndarray:
+        return user_row @ self.B
