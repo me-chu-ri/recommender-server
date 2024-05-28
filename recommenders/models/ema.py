6ec829c759da3d78e2a5a33866be252d4f8c8ee1
Park-SeungWoo <tmd24710@naver.com>
2024-05-28 18:54:51 +0900

    feat: add adjusting logic in t1 to EMA model

--- recommenders/models/ema.py
+++ recommenders/models/ema.py
@@ -2,5 +2,7 @@ class EMA:
     def __init__(self, beta: float = 0.7):
         self.beta = beta
 
-    def update(self, old: float, new: float) -> float:
+    def update(self, old: float, new: float, is_t1: bool = False) -> float:
+        if is_t1:
+            return new
         return old * self.beta + new * (1 - self.beta)
